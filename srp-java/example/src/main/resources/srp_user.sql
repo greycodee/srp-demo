f466d91d2c124f1dbc11f8c9f83512b56ccbef1a
greycodee <zhengminghui99@gmail.com>
2021-09-20 22:03:51 +0800

    delete srp-java

--- srp-java/example/src/main/resources/srp_user.sql
+++
@@ -1,6 +0,0 @@
-create table srp_user(
-                         id int auto_increment primary key ,
-                         user_name varchar(255) not null comment '用户名',
-                         user_v varchar(255) not null comment '验证值 v',
-                         salt varchar(255) not null comment '加密盐值'
-)
